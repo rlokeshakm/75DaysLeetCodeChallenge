c13a8aaa269182ebcd7d08ac7a62b13f94361b69
rlokeshakm <rlokeshakm0@gmail.com>
2026-04-02 01:13:19 +0530

    Time: 3 ms (57.11%), Space: 20.6 MB (84.14%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,22 +1,23 @@
 class Solution:
-    def resolves(self, a, b, Operator):
-        if Operator == '+':
-            return a + b
-        elif Operator == '-':
-            return a - b
-        elif Operator == '*':
-            return a * b
-        return int(a / b)
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack=[]
+        for i in tokens:
+            if i == "+":
+                stack.append(stack.pop()+stack.pop())
 
-    def evalRPN(self, tokens):
-        stack = []
-        for token in tokens:
-            if len(token) == 1 and ord(token) < 48:
-                integer2 = stack.pop()
-                integer1 = stack.pop()
-                operator = token
-                resolved_ans = self.resolves(integer1, integer2, operator)
-                stack.append(resolved_ans)
+            elif i == "-":
+                a,b=stack.pop(),stack.pop()
+                stack.append(b-a)
+
+            elif i == "*":
+                stack.append(stack.pop()*stack.pop())
+
+            elif i == "/":
+                a,b=stack.pop(),stack.pop()
+                stack.append(int(b/a))
+
             else:
-                stack.append(int(token))
-        return stack.pop()
+                stack.append(int(i))
+        return stack[-1]
+
+        
